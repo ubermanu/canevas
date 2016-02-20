9ca61cdb1c26ce1f7b7fb44eb69f80ad7afe3eda
Manu <manu@asherontas>
2016-02-20 23:15:26 +0100

    removed scene functions in canvas and implements scene class

--- src/core/Canvas.js
+++ src/core/Canvas.js
@@ -2,15 +2,14 @@ SILK.Canvas = function () {
 	
 	console.log( 'SILK.Canvas', SILK.VERSION );
 	
-	var _canvas = document.createElement( 'canvas' );
+	var _canvas  = document.createElement( 'canvas' );
 	var _context = _canvas.getContext( '2d' );
 	
 	// public properties
 	
 	this.domElement = _canvas;
-	this.context = null;
-	this.autoClear = true;
-	this.children = [];
+	this.context 	= null;
+	this.autoClear 	= true;
 	
 	this.setSize = function ( width, height ) {
 		
@@ -27,47 +26,21 @@ SILK.Canvas = function () {
 		_context.clearRect( 0, 0, _canvas.width, _canvas.height );
 	};
 	
-	// add child
-	
-	this.add = function ( object ) {
-		
-		if ( object instanceof SILK.Object2D ) {
-			
-			if ( object.parent !== null ) {
-				object.parent.remove( object );
-			}
-			
-			object.parent = this;
-			this.children.push( object );
-			
-		} else {
-			
-			console.error( "SILK.Canvas.add: object is not an instance of SILK.Object2D", object );
-		}
-		
-		return this;
-	};
-	
-	// remove child
-	
-	this.remove = function ( object ) {
-		
-		var index = this.children.indexOf( object );
-		
-		if ( index !== - 1 ) {
-			object.parent = null;
-			this.children.splice( index, 1 );
-		}
-	};
-	
 	// render
 	
 	this.render = function ( scene ) {
 		
-		if ( this.autoClear == true ) this.clear();
-		
-		for ( var i = 0; i < this.children.length; i++ ) {
-			this.children[ i ].render( _context );
+		if ( scene instanceof SILK.Scene ) {
+			
+			if ( this.autoClear == true ) this.clear();
+			
+			for ( var i = 0; i < scene.children.length; i++ ) {
+				scene.children[ i ].render( _context );
+			}
+			
+		} else {
+			
+			console.error( "SILK.Canvas.render: scene is not an instance of SILK.Scene", scene );
 		}
 	};
 }
