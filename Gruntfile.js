33fd3561a8f76c08f6b14666c521240b9a4e6df1
Manu <manu@asherontas>
2016-02-20 23:14:11 +0100

    added build options and include source for test files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,48 @@
-module.exports = function(grunt) {
+module.exports = function ( grunt ) {
 
 	// Project configuration.
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			concat: {
-				src: ['src/**/*'],
-				dest: 'build/silk.min.js',
+		
+		pkg: grunt.file.readJSON( 'package.json' ),
+		
+		concat: {
+			dist: {
+				src: [ 'src/**/*' ],
+				dest: 'build/silk.js',
 				filter: 'isFile'
 			},
 			options: {
 				banner: '/*! silk.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
+			}
+		},
+		
+		uglify: {
 			build: {
 				src: 'build/silk.js',
 				dest: 'build/silk.min.js'
+			},
+			options: {
+				banner: '/*! silk.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			}
+		},
+		
+		includeSource: {
+			options: {
+				basePath: '',
+				baseUrl: '../'
+			},
+			testTarget: {
+				files: {
+					'tests/test-scene.html': 'tests/test-scene.html'
+				}
 			}
 		}
 	});
 
-	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-include-source');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['concat', 'uglify', 'includeSource']);
 };
