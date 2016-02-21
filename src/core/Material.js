11434fb99127bfdb5a2c58fbc1725097c11a9309
Manu <e.vodor@gmail.com>
2016-02-21 11:52:45 +0100

    added material and shape to core

---
+++ src/core/Material.js
@@ -0,0 +1,14 @@
+SILK.Material = function ( options ) {
+	
+	options = options || {};
+	this.type = 'Material';
+};
+
+SILK.Material.prototype = {
+	
+	constructor: SILK.Material,
+	
+	render: function ( context ) {
+		
+	}
+};
---
+++ src/core/Shape.js
@@ -0,0 +1,14 @@
+SILK.Shape = function ( options ) {
+	
+	options = options || {};
+	this.type = 'Shape';
+};
+
+SILK.Shape.prototype = {
+	
+	constructor: SILK.Shape,
+	
+	render: function ( context ) {
+		
+	}
+};
