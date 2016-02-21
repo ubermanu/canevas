64251bd7192ee980e755c04d9419875c710d9a17
Manu <e.vodor@gmail.com>
2016-02-21 11:49:44 +0100

    added  functions add/remove child

--- src/core/Object2D.js
+++ src/core/Object2D.js
@@ -6,32 +6,48 @@ SILK.Object2D = function ( options ) {
 	
 	this.type 		= 'Object2D';
 	this.parent 	= null;
+	this.children 	= [];
 	
 	this.position 	= options.position !== undefined ? options.position : new SILK.Vector2();
 	this.rotation 	= options.rotation !== undefined ? options.rotation : 0;
-	this.scale 		= options.scale !== undefined ? options.scale : 1;
+	this.scale 		= options.scale    !== undefined ? options.scale : 1;
 	
-	this.visible 	= options.visible !== undefined ? options.visible : true;
-	this.wireframe 	= options.wireframe !== undefined ? options.wireframe : false;
-	
-	var _color 		= options.color !== undefined ? options.color : 0x000000;
-	this.color 		= new SILK.Color( _color );
+	this.visible 	= options.visible  !== undefined ? options.visible : true;
 };
 
 SILK.Object2D.prototype = {
 	
 	constructor: SILK.Object2D,
 	
-	render: function ( context ) {
+	add: function ( object ) {
 		
-		if ( ! this.visible ) return;
+		if ( object instanceof SILK.Object2D ) {
+			
+			if ( object.parent !== null ) {
+				
+				object.parent.remove( object );
+			}
+			
+			object.parent = this;
+			this.children.push( object );
+			
+		} else {
+			
+			console.error( "SILK.Object2D.add: object is not an instance of SILK.Object2D", object );
+		}
 		
-		if ( this.rotation > Math.PI * 2 ) this.rotation = 0;
+		return this;
+	},
+	
+	remove: function ( object ) {
 		
-		context.save();
-		context.scale( this.scale, this.scale );
-		context.translate( this.position.x, this.position.y );
-		context.rotate( this.rotation );
+		var index = this.children.indexOf( object );
+		
+		if ( index !== - 1 ) {
+			
+			object.parent = null;
+			this.children.splice( index, 1 );
+		}
 	}
 }
 
