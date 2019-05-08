4f415ab0bad9e326badafb912e51204ba5f97855
ubermanu <e.vodor@gmail.com>
2019-05-08 13:12:40 +0200

    Export a minified version of the lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,28 @@
 import typescript from 'rollup-plugin-typescript';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
-  input: 'src/index.ts',
-  plugins: [
-    typescript()
-  ],
-  output: [
-    {
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript()
+    ],
+    output: {
       format: 'umd',
-      name: 'SILK',
-      file: 'dist/silk.js'
+      name: 'ng',
+      file: 'dist/ng.js'
     }
-  ]
-}
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript(),
+      uglify()
+    ],
+    output: {
+      format: 'umd',
+      name: 'ng',
+      file: 'dist/ng.min.js'
+    }
+  }
+];
