1de4bd935624cb1fe70e886163c695aedaec88ce
ubermanu <manu@paca.happy-dev.fr>
2021-05-01 11:39:59 +0200

    Export as an ES module

--- rollup.config.js
+++ rollup.config.js
@@ -5,27 +5,10 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      format: 'umd',
-      name: 'cc',
-      file: 'dist/canevas.js',
-    },
-    plugins: [typescript()],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      format: 'umd',
-      name: 'cc',
-      file: 'dist/canevas.min.js',
-    },
-    plugins: [typescript(), terser()],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
       format: 'es',
       file: 'dist/canevas.mjs',
+      sourcemap: true,
     },
-    plugins: [typescript()],
+    plugins: [typescript(), terser()],
   },
 ]
