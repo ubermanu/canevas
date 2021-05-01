28ab629c42af153541e1498ef433c6789616dbee
ubermanu <manu@paca.happy-dev.fr>
2021-05-01 11:31:28 +0200

    Format code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-export * from "./core/Camera";
-export * from "./core/Canvas";
-export * from "./core/Material";
-export * from "./core/Object2D";
-export * from "./core/Shape";
+export * from './core/Camera'
+export * from './core/Canvas'
+export * from './core/Material'
+export * from './core/Object2D'
+export * from './core/Shape'
 
-export * from "./materials/BasicMaterial";
-export * from "./materials/ImageMaterial";
-export * from "./materials/SpriteMaterial";
+export * from './materials/BasicMaterial'
+export * from './materials/ImageMaterial'
+export * from './materials/SpriteMaterial'
 
-export * from "./math/Vector2";
-export * from "./math/Color";
+export * from './math/Vector2'
+export * from './math/Color'
 
-export * from "./objects/Mesh";
-export * from "./objects/Scene";
+export * from './objects/Mesh'
+export * from './objects/Scene'
 
-export * from "./shapes/BoxShape";
-export * from "./shapes/CircleShape";
-export * from "./shapes/PolygonShape";
-export * from "./shapes/TriangleShape";
+export * from './shapes/BoxShape'
+export * from './shapes/CircleShape'
+export * from './shapes/PolygonShape'
+export * from './shapes/TriangleShape'
