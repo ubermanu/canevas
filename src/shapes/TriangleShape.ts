466ceebb2705647ddf1e720e937681022cb5f275
ubermanu <manu@paca.happy-dev.fr>
2022-03-01 14:10:43 +0100

    block triangle shape faces to 3

--- src/shapes/TriangleShape.ts
+++ src/shapes/TriangleShape.ts
@@ -9,6 +9,13 @@ export class TriangleShape extends PolygonShape {
   constructor(options: TriangleShapeOptions = {}) {
     super({ ...options, faces: 3 })
   }
+
+  set faces(value: number) {
+    if (value !== 3) {
+      throw new Error('TriangleShape can only have 3 faces.')
+    }
+    this._faces = value
+  }
 }
 
 export interface TriangleShapeOptions {
