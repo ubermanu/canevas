20be334417db800e98187f2d00b2f7012e7acca6
ubermanu <manu@paca.happy-dev.fr>
2022-03-01 12:00:55 +0100

    build ts types

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,30 @@
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
+import dts from 'rollup-plugin-dts'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'es',
+        file: 'dist/canevas.mjs',
+        sourcemap: true,
+      },
+      {
+        format: 'cjs',
+        file: 'dist/canevas.cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript(), terser()],
+  },
+  {
+    input: 'types/index.d.ts',
+    output: {
+      file: 'dist/canevas.d.ts',
       format: 'es',
-      file: 'dist/canevas.mjs',
-      sourcemap: true,
     },
-    {
-      format: 'cjs',
-      file: 'dist/canevas.cjs',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript(), terser()],
-}
+    plugins: [dts()],
+  },
+]
